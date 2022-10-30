7f150a74de8cbaff0e26a56f5a814cce3f78fc45
Aleksandr Tsertkov <tsertkov@gmail.com>
2022-10-30 19:50:58 +0100

    Add missing quotes in aws-secrets.sh

--- scripts/aws-secrets.sh
+++ scripts/aws-secrets.sh
@@ -11,13 +11,13 @@ region=$3
 app=$4
 secrets_file=$5
 
-if [ $operation = "delete" ]; then
+if [ "$operation" = "delete" ]; then
 	echo "Deleting secrets with prefix ${secret_prefix} in ${region} region"
 	for key in $(aws secretsmanager list-secrets --filters Key=name,Values=${secret_prefix} --query 'SecretList[].Name' --output text); do
 		aws secretsmanager delete-secret --region "$region" --secret-id "$key" --force-delete-without-recovery
 	done
 
-elif [ $operation = "update" ]; then
+elif [ "$operation" = "update" ]; then
 	if [ ! -f "$secrets_file" ]; then
 		echo "Secrets file does not exists: '$secrets_file'"
 		exit
@@ -25,7 +25,7 @@ elif [ $operation = "update" ]; then
 
 	secrets="$(yq ".${app}" "$secrets_file")"	
 
-	if [ $secrets = "null" ]; then
+	if [ "$secrets" = "null" ]; then
 		echo "No secrets to create or update for ${app} in ${region}"
 		exit
 	fi
@@ -33,8 +33,8 @@ elif [ $operation = "update" ]; then
 	echo "Updating secrets for ${app} in ${region}"
 
 	for secret in "$secrets"; do
-		key=${secret_prefix}$(echo "$secret" | sed "s/:.*$//")
-		value=$(echo "$secret" | sed "s/^.*: //")
+		key="${secret_prefix}$(echo "$secret" | sed "s/:.*$//")"
+		value="$(echo "$secret" | sed "s/^.*: //")"
 
 		aws secretsmanager create-secret --region "$region" --name "$key" --secret-string "$value" 2>/dev/null \
 			|| aws secretsmanager update-secret --region "$region" --secret-id "$key" --secret-string "$value"
